2112b0d5139f61e74562939a05eb811f45948c40
davidbaba <david.baba@seerbit.com>
2024-04-15 10:48:39 +0100

    Initial commit

---
+++ src/main/java/com/davacom/testingd/TestingDApplication.java
@@ -0,0 +1,13 @@
+package com.davacom.testingd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingDApplication.class, args);
+    }
+
+}
